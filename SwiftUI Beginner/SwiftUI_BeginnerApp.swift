7374db7b8d79d7b5659fd0512101810d4f1ecd49
macbookpro <munjur533@gmail.com>
2022-07-07 10:27:41 +0600

    Initial Commit

---
+++ SwiftUI Beginner/SwiftUI_BeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BeginnerApp.swift
+//  SwiftUI Beginner
+//
+//  Created by MacBook Pro on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
